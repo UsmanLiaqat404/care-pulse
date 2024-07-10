9d6c9ad93ba99108fb78b395d684245d30ec12bc
Usman Liaqat <memerboyemiology444@gmail.com>
2024-07-10 10:20:29 +0500

    chore: Add getPatient function to retrieve patient information

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import AppointmentForm from "@/components/form/AppointmentForm";
+import { getPatient } from "@/lib/actions/patient.actions";
+import Image from "next/image";
 
-const NewAppointment = () => {
-  return <div>New Appointment</div>;
-};
+export default async function NewAppointment({
+  params: { userId },
+}: SearchParamProps) {
+  const patient = await getPatient(userId);
+  console.log(patient, "patient");
 
-export default NewAppointment;
+  return (
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[860px] flex-1 justify-between">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <AppointmentForm
+            type="create"
+            userId={userId}
+            patientId={patient.$id}
+          />
+
+          <p className="copyright mt-10 my-12">
+            © {new Date().getFullYear()} CarePulse
+          </p>
+        </div>
+      </section>
+
+      <Image
+        src="/assets/images/appointment-img.png"
+        height={1000}
+        width={1000}
+        alt="appointment"
+        className="side-img max-w-[390px] bg-bottom"
+      />
+    </div>
+  );
+}
